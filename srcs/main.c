dfad4839432567118d2d8006c9598bd867adb179
Arthur BONNECARRERE <abonneca@e3r4p19.42.fr>
2017-02-07 16:44:58 +0100

    yo

--- srcs/main.c
+++ srcs/main.c
@@ -36,10 +36,12 @@ size_t ft_push_box(t_inf	*inf, t_box *box, char *name, char *pos)
 {
 
 	t_box *first;
+	t_box *new_box;
 
 	if (!(new_box = (t_box *)malloc(sizeof(t_box))))
 		return (1);
-		first = box;
+	first = box;
+	box->name = ft_strdup(name);
 	if (!ft_strcmp(pos, "start"))
 		inf->start = name;
 	else if(!ft_strcmp(pos, "end"))
@@ -48,8 +50,10 @@ size_t ft_push_box(t_inf	*inf, t_box *box, char *name, char *pos)
 		box = box->next;
 	if (box == NULL)
 		box = new_box;
-
-	box = first;
+	else
+		box = first;
+	printf("push box->name: %s\n", name);
+	return (0);
 }
 
 size_t	ft_tagline(char **line,	t_inf	*inf, t_box *box)
@@ -59,14 +63,17 @@ size_t	ft_tagline(char **line,	t_inf	*inf, t_box *box)
 	if ((ft_strcmp(line[0], "##start") == 0 || ft_strcmp(line[0], "##end") == 0 )
 && get_next_line(0, &str) > 0)
 	{
-		line = ft_strsplit(str, ' ');
+		printf("%s\n", line[0]);
 		if (ft_strtab(line) != 3 || line[0][0] == 'L')
 			return (1);
 		if (ft_strcmp(line[0], "##start") == 0)
+		{
+			line = ft_strsplit(str, ' ');
 			if (ft_push_box(inf, box, line[0], "start"))
 				return(1);
+		}
 		else if (ft_push_box(inf, box, line[0], "end"))
-				return(1);
+			return(1);
 	}
 	return (0);
 }
@@ -102,13 +109,6 @@ int main(void)
 		line = ft_strsplit(str, ' ');
 		if (ft_strtab(line) < 1)
 			break;
-		// if (line[0][0] == 'L')
-		// if (ft_strrchr(line, '-'))
-		if (!first_line && ft_isnumber(line[0]) && ft_strtab(line) == 1)
-		{
-				inf->lemmings = ft_atoi(line[0]);
-				first_line = 1;
-		}
 			// printf("line[0]: %s\n", line[0]);
 			// printf("first_line: %i\n", first_line);
 			// printf("ft_strcmp(line[0], #): %i\n", line[0][0] == "#"));
@@ -126,9 +126,19 @@ int main(void)
 			if (ft_pipe(line, inf, box))
 				break;
 		}
+		if (!first_line && ft_isnumber(line[0]) && ft_strtab(line) == 1)
+		{
+		// printf("yo");
+				inf->lemmings = ft_atoi(line[0]);
+				first_line = 1;
+		}
 		}
 	// printf("%zu\n", inf->lemmings);
-	// printf("%s\n", inf->start);
+	while (box)
+{
+	printf("box->name: %s\n", box->name);
+	box = box->next;
+}
 		if (!inf->valid_map)
 			printf("ERROR");
 	return (0);
