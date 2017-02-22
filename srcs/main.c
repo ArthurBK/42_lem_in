6efb4cca53a4b31902e35631e411f6ab908e6e7c
Arthur BONNECARRERE <abonneca@e2r3p16.42.fr>
2017-02-22 13:38:13 +0100

    to norm

--- srcs/main.c
+++ srcs/main.c
@@ -6,69 +6,58 @@
 /*   By: abonneca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/05 14:44:36 by abonneca          #+#    #+#             */
-/*   Updated: 2017/02/22 11:34:33 by abonneca         ###   ########.fr       */
+/*   Updated: 2017/02/22 13:25:13 by abonneca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-
-int	ft_parse(t_inf *inf, t_box **box, t_path **paths)
+int			ft_parse(t_inf *inf, t_box **box, t_path **paths)
 {
-	char	first_line;
+	char	f_l;
 	char	*str;
-	char	**line;
+	char	**l;
 
 	while (get_next_line(0, &str) > 0)
 	{
-		line = ft_strsplit(str, ' ');
-		if (first_line && ft_strtab(line) > 0 && line[0][0] == '#' && ft_tagline(line, &inf, box))
-				return (1);
-		else if (first_line && ft_strtab(line) == 3 && ft_push_box(&inf, box, line[0], "box"))
-				return (1);
-		if (first_line && ft_strtab(line) == 1 && line[0][0] != '#')
+		l = ft_strsplit(str, ' ');
+		if (f_l && ft_strtab(l) > 0 && l[0][0] == '#' && ft_tagline(l, &inf, box))
+			return (0);
+		else if (f_l && ft_strtab(l) == 3 && ft_push_box(&inf, box, l[0], "box"))
+			return (1);
+		if (f_l && ft_strtab(l) == 1 && l[0][0] != '#')
 		{
-			free_lines(line);
-			line = ft_strsplit(str, '-');
-			if (ft_pipe(line, &inf, box))
+			free_lines(l);
+			if (ft_pipe(l = ft_strsplit(str, '-'), &inf, box))
 				break ;
 		}
-		if (!first_line && ft_isnumber(line[0]) && ft_strtab(line) == 1)
-		{
-			inf->lemmings = ft_atoi(line[0]);
-			first_line = 1;
-		}
-		free_lines(line);
+		if (!f_l && ft_isnumber(l[0]) && ft_strtab(l) == 1
+				&& (inf->lemmings = ft_atoi(l[0])))
+			f_l = 1;
+		free_lines(l);
 		free(str);
 	}
 	free(str);
-	return(0);
+	return (0);
 }
 
 int		main(void)
 {
-	// char	*str;
-	// int		ret;
 	t_inf	*inf;
 	t_box	*box;
 	t_path	*paths;
 
 	box = NULL;
 	paths = NULL;
-	// first_line = (char *)malloc(sizeof(char));
 	inf = (t_inf *)malloc(sizeof(t_inf));
 	ft_bzero(inf, sizeof(inf));
-	// first_line = 0;
 	if (ft_parse(inf, &box, &paths))
-		return (0);
-
+		return (1);
 	paths = ft_find_paths(&box, &inf);
 	ft_lem_in(&paths, &inf);
 	free(inf->start);
 	free(inf->end);
 	free(inf);
-	// printf("out of parse\n");
-
 	sleep(7);
 	return (0);
 }
